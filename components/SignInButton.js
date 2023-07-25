34db87cd5e69532c01e7a8d935e650ac7fb90f3e
Amish Verma <amishhverma@gmail.com>
2023-07-25 20:16:04 +0530

    Added automatic navigation to homepage after login or logout & Made some changes in CSS of these components

--- components/SignInButton.js
+++ components/SignInButton.js
@@ -1,11 +1,17 @@
 import { signInWithGoogle } from "../lib/firebase";
+import { useRouter } from "next/router";
 
 const SignInButton = () => {
+  const router = useRouter();
+
   return (
-    <div>
+    <div className="mt-24">
       <button
-        className="bg-blue-800 text-white p-3 rounded-lg"
-        onClick={() => signInWithGoogle()}
+        className="bg-blue-800 text-xl text-white px-5 py-4 rounded-lg"
+        onClick={() => {
+          signInWithGoogle();
+          router.push("/");
+        }}
       >
         Sign In With Google
       </button>
--- components/SignOutButton.js
+++ components/SignOutButton.js
@@ -1,11 +1,17 @@
 import { logOut } from "../lib/firebase";
+import { useRouter } from "next/router";
 
 const SignOutButton = () => {
+  const router = useRouter();
+
   return (
     <div>
       <button
         className="bg-blue-800 text-white p-3 rounded-lg"
-        onClick={() => logOut()}
+        onClick={() => {
+          logOut();
+          router.push("/");
+        }}
       >
         Sign Out
       </button>
