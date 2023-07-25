da153ad237959f2e60a8d62fcac4ff887b80b320
Amish Verma <amishhverma@gmail.com>
2023-07-25 19:00:29 +0530

    Added modal view of Navbar menu for mobile devices

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,16 @@
 import Link from "next/link";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../lib/context";
 import SignOutButton from "./SignOutButton";
 
 const Navbar = () => {
   const { user, username } = useContext(UserContext);
-  
+  const [showModal, setShowModal] = useState("hidden");
+
+  const handleNav = () => {
+    setShowModal((prevValue) => (prevValue === "flex" ? "hidden" : "flex"));
+  };
+
   return (
     <nav className="flex justify-between p-5 text-xl border-b-2 md:max-w-7xl mx-auto">
       <div>
@@ -15,7 +20,9 @@ const Navbar = () => {
           </button>
         </Link>
       </div>
-      <ul className="flex gap-4">
+
+      {/* Render when screen-size is big */}
+      <ul className="md:flex md:gap-4 hidden">
         {username && (
           <>
             <li>
@@ -49,6 +56,53 @@ const Navbar = () => {
             </Link>
           </li>
         )}
+      </ul>
+
+      {/* Render modal for mobile devices */}
+      <ul className="md:hidden flex">
+        <button onClick={handleNav}>
+          <img
+            className="border-2 border-blue-400 hover:border-blue-600 w-14 aspect-square object-cover rounded-full"
+            src={user?.photoURL}
+            alt=""
+          />
+        </button>
+        <div
+          className={`${showModal} absolute top-[5.5rem] right-8 p-6 rounded-2xl bg-gray-200`}
+        >
+          <div className="flex items-end flex-col gap-2">
+            {!username && (
+              <li>
+                <Link href="/enter">
+                  <button className="bg-blue-800 text-white p-3 rounded-lg">
+                    Log In
+                  </button>
+                </Link>
+              </li>
+            )}
+            {username && (
+              <>
+                <li>
+                  <Link href={`/${username}`}>
+                    <button className="bg-blue-800 text-white p-3 rounded-lg">
+                      My Account
+                    </button>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/admin">
+                    <button className="bg-blue-800 text-white p-3 rounded-lg">
+                      Write Posts
+                    </button>
+                  </Link>
+                </li>
+                <li>
+                  <SignOutButton />
+                </li>
+              </>
+            )}
+          </div>
+        </div>
       </ul>
     </nav>
   );
