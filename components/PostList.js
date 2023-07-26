38e92bb76f00552ef5b1d5d1907ab500cc854e0e
Amish Verma <amishhverma@gmail.com>
2023-07-26 11:56:33 +0530

    Fixed postlist component error when users signs out from write posts page
    The posts object would become null when the user logs out so when next.js tries to re-render the page, an error is thrown. Fixed this issue.

--- components/PostList.js
+++ components/PostList.js
@@ -5,16 +5,23 @@ import { useCollection } from "react-firebase-hooks/firestore";
 import PostFeed from "../components/PostFeed";
 
 const PostList = () => {
-  const postsRef = collection(db, "users", auth.currentUser.uid, "posts");
-  const q = query(postsRef, orderBy("createdAt"));
-  const [querySnapshot] = useCollection(q);
+  let posts;
+  try {
+    const postsRef = collection(db, "users", auth.currentUser.uid, "posts");
+    const q = query(postsRef, orderBy("createdAt"));
+    const [querySnapshot] = useCollection(q);
 
-  const posts = querySnapshot?.docs.map((doc) => doc.data());
+    posts = querySnapshot?.docs.map((doc) => doc.data());
+  } catch (error) {
+    console.log(error.message);
+  }
 
   return (
     <div>
-      <p className="text-3xl font-bold md:max-w-4xl mx-4 md:mx-auto mt-12 mb-4 border-t pt-4">Manage Your Posts</p>
-      <PostFeed posts={posts} admin />
+      <p className="text-3xl font-bold md:max-w-4xl mx-4 md:mx-auto mt-12 mb-4 border-t pt-4">
+        Manage Your Posts
+      </p>
+      {posts && <PostFeed posts={posts} admin />}
     </div>
   );
 };
